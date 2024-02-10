5bad6bfd40208d32191b296ebe0f4dc097a6085e
victor <victordj587@gmail.com>
2024-02-10 20:40:36 +0100

    init

---
+++ postcss.config.cjs
@@ -0,0 +1,15 @@
+//ts-ignore
+
+const tailwindcss = require("tailwindcss");
+const autoprefixer = require("autoprefixer");
+
+const config = {
+  plugins: [
+    //Some plugins, like tailwindcss/nesting, need to run before Tailwind,
+    tailwindcss(),
+    //But others, like autoprefixer, need to run after,
+    autoprefixer,
+  ],
+};
+
+module.exports = config;
