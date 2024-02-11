41ff8a1708e5a7962e764a887fafb08463b96026
victor <victordj587@gmail.com>
2024-02-11 16:25:25 +0100

    Add firebase initialization in GET request handler

--- src/routes/a/[id]/+server.ts
+++ src/routes/a/[id]/+server.ts
@@ -2,9 +2,11 @@ import type { RequestHandler } from './$types';
 import { redirect } from '@sveltejs/kit';
 import { Redirect } from '@sveltejs/kit';
 import { getFirestore, collection, query, where, getDocs } from "firebase/firestore";
+import { firebaseInit } from '$lib/js/firebase';
 
 
 export const GET: RequestHandler = async ({ url }) => {
+    firebaseInit();
     let uid = url.pathname.split('/').pop();
     const db = getFirestore();
     const q = query(collection(db, "posts"), where("uid", "==", uid));
