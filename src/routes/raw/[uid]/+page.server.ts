f2111fa27a823a3691ebbf981640479d9580f915
victor <victordj587@gmail.com>
2024-03-05 19:33:37 +0100

    Refactor database query in +page.server.ts

--- src/routes/raw/[uid]/+page.server.ts
+++ src/routes/raw/[uid]/+page.server.ts
@@ -1,40 +1,14 @@
 import type { Post } from "$lib/types/post";
+import { sql } from "@vercel/postgres";
 import type { PageServerLoad } from "./$types";
-import {
-  getFirestore,
-  collection,
-  query,
-  where,
-  getDocs,
-} from "firebase/firestore";
 
 export const load = (async ({ url }) => {
+  const start = Date.now();
   let uid = url.pathname.split("/").pop();
 
-  const db = getFirestore();
-  const q = query(collection(db, "posts"), where("uid", "==", uid));
-  const querySnapshot = await getDocs(q);
-  let post: Post = {
-    AIsummary: "",
-    author: "",
-    authorUUID: "",
-    content: "",
-    coverImage: "",
-    coverImageAlt: "",
-    coverImageCaption: "",
-    coverImageCredit: "",
-    date: "",
-    minToRead: 0,
-    slug: "",
-    subTitle: "",
-    title: "",
-    topic: "",
-    topicColor: "",
-    uid: "",
-  }; // Initialize post variable with an empty object
-  querySnapshot.forEach((doc) => {
-    post = doc.data() as Post;
-  });
+  const { rows } = await sql`SELECT * FROM Posts WHERE uid = ${uid}`;
+  let post = rows[0] as Post;
+  const end = Date.now();
 
-  return { uid, post };
+  return { uid, post, time: end - start };
 }) satisfies PageServerLoad;
