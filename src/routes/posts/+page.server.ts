bd5f90d0732f07e3f8bcb84f455e37be66ee5ab8
Victor Digital <victordj587@gmail.com>
2024-03-07 11:32:15 +0100

    Strip content and cover image links from posts list

--- src/routes/posts/+page.server.ts
+++ src/routes/posts/+page.server.ts
@@ -8,5 +8,17 @@ export const load = (async () => {
   const { rows } = await sql`SELECT * FROM Posts`;
   let posts = rows as Post[];
   const end = Date.now();
+
+  //before returning the posts, we need to strip the content from the posts
+  posts = posts.map((post) => {
+    return {
+      ...post,
+      content: "",
+      coverimagelinkavif: "",
+      coverimagelinkwebp: "",
+      coverimagelinkpng: "",
+    };
+  });
+
   return { posts, time: end - start };
 }) satisfies PageServerLoad;
